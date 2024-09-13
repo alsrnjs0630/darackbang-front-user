e59c821dd64babdd043dbefb302e7fabb335f34c
mankatos <klaatus@msn.com>
2024-09-13 09:27:46 +0900

    사용자 화면용 업로드

--- src/layouts/ProductLayout.js
+++ src/layouts/ProductLayout.js
@@ -8,6 +8,7 @@ const BasicLayout = ({children}) => {
         <>
             <BasicMenu/>
             <div className={'bg-white my-5 w-full flex flex-col space-y-1 md:flex-row md:space-x-1 md:space-y-0'}>
+                <SideBarMenu/>
                 <main className={'bg-sky-300 md:w-4/5 lg:w-3/4 px-5 py-5'}>
                     {children}
                 </main>
---
+++ src/pages/ProductPage.js
@@ -0,0 +1,14 @@
+import ProductLayout from '../layouts/ProductLayout';
+
+const ProductPage = () => {
+
+	return (
+		<ProductLayout>
+			<div className={'text-3xl'}>
+				<div> 상품 리스트 출력</div>
+			</div>
+		</ProductLayout>
+	);
+}
+
+export default ProductPage;
